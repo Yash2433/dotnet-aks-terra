8b752348ae457940254e7098db2f0b389385d2bd
Yash2433 <yashmadmaxfoe@gmail.com>
2025-04-17 15:30:57 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,41 +3,50 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-resource "azurerm_resource_group" "rg"{
-    name = var.resource_group_name
-    location = var.location
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_container_registry" "acr" {
-  resource_group_name = var.resource_group_name
-  location = var.location
-  name = var.acr_name
-  sku = "Basic"
-  admin_enabled = true
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  name                = var.acr_name
+  sku                 = "Basic"
+  admin_enabled       = true
 }
 
-
-//az aks create --resource-group rg-docker --name aks-dotnetwebapi --node-count 2 --node-vm-size Standard_B2ms --generate-ssh-keys --attach-acr acrkeshav070425
 resource "azurerm_kubernetes_cluster" "aks" {
-  location = var.location
-  resource_group_name = var.resource_group_name
-  name = var.aks_name
-  dns_prefix = var.dns_prefix
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  name                = var.aks_name
+  dns_prefix          = var.dns_prefix
 
   default_node_pool {
-    vm_size = "Standard_B2ms"
+    vm_size   = "Standard_B2ms"
     node_count = 2
-    name = "default"
+    name      = "default"
   }
-  
+
   identity {
     type = "SystemAssigned"
   }
-  
-  depends_on = [ azurerm_container_registry.acr ]
-  
+
+  depends_on = [azurerm_container_registry.acr]
+
   network_profile {
-    network_plugin = "azure"
+    network_plugin    = "azure"
     load_balancer_sku = "standard"
   }
+
+  # Enabling ACR integration with AKS
+  addon_profile {
+    acr {
+      enabled = true
+      config {
+        acr_name = azurerm_container_registry.acr.name
+      }
+    }
+  }
 }
+
