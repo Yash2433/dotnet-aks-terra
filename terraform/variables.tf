dcee17b58652b1ee3161f4d8b066973318c77cab
Yash2433 <yashmadmaxfoe@gmail.com>
2025-04-17 15:34:13 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,39 +1,41 @@
 variable "subscription_id" {
-  description = "Subscription id of the account"
-  type = string
-  default = "a98d0e19-0c0e-4a5b-91ac-c6923c6331bc"
+  description = "Azure Subscription ID"
+  type        = string
+  default     = "a98d0e19-0c0e-4a5b-91ac-c6923c6331bc"
 }
 
 variable "location" {
-    description = "location of service"
-    type = string
-    default = "eastus"
+  description = "Azure Location where resources will be deployed"
+  type        = string
+  default     = "eastus"
 }
 
 variable "resource_group_name" {
-  description = "resource group name"
-  type = string
-  default = "myResourceGroup"
+  description = "Name of the Resource Group"
+  type        = string
+  default     = "myResourceGroup"
 }
 
 variable "acr_name" {
-  description = "Name of the service plan"
-  type = string
-  default = "myacryash"
+  description = "Name of the Azure Container Registry"
+  type        = string
+  default     = "myacryash"
 }
 
 variable "os" {
-  description = "Operating system"
-  type = string
-  default = "Linux"
+  description = "Operating system for AKS (Linux/Windows)"
+  type        = string
+  default     = "Linux"
 }
 
 variable "aks_name" {
-    description = "Name of the service plan"
-    type = string
-    default = "myAKSCluster"
+  description = "Name of the Azure Kubernetes Service Cluster"
+  type        = string
+  default     = "myAKSCluster"
 }
 
-variable "dns_prefix"{
-  default = "my-akscluster"
+variable "dns_prefix" {
+  description = "DNS Prefix for AKS"
+  type        = string
+  default     = "my-akscluster"
 }
